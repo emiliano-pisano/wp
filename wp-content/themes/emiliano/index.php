6daeebf07e763c6b29a11c294fa77c6e2737e720
emiliano-pisano <pisanoemiliano@gmail.com>
2017-05-21 12:46:51 +0200

    Add files via upload

---
+++ wp-content/themes/emiliano/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="vendors/css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+  <div class="jumbotron">
+    <div class="headline">
+      I fiori sono gli occhi con cui guardare la natura
+    </div>
+    <p><nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"></a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Chi siamo</a></li>
+            <li><a href="#">Galleria</a></li>
+            <li><a href="#">Shop online</a></li>
+            <li><a href="#contatti">Contattaci</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav></p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Il negozio</a></p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Cerimonie</a></p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Bonsai</a></p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">I nostri lavori</a></p>
+  </div>
+
+  <!-- form begin -->
+  <div class="container-fluid" id="contatti">
+
+    <div class="row text-center">
+      <h1> Dove siamo <h1>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3090.9707300822975!2d9.124612715004783!3d39.22083047952208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e73472652cb289%3A0xd034f378ed63f352!2sVia+Giuseppe+Verdi%2C+19%2C+09128+Cagliari!5e0!3m2!1sit!2sit!4v1495143545414" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <h3> Inforidea Via G.Verdi, 19 09100 Cagliari <h3> 
+          <h1> Contattaci <h1> 
+          </div>
+
+          <form class="pisano-form">
+
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="col-lg-6">
+
+                  <div class="form-group">
+                    <label for="exampleInputNome">Nome</label>
+                    <div class="input-group">
+                      <span class="input-group-addon" id="basic-addon1"><i class="glyphicon glyphicon-user"></i></span>
+                      <input type="text" class="form-control" placeholder="Digita qui il tuo nome" required>
+                    </div>
+
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Indirizzo email</label>
+                      <div class="input-group">
+                        <span class="input-group-addon" id="basic-addon1"><i class="glyphicon glyphicon-envelope"></i></span>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digita qui la tua email" required>
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <label for="exampleInputTelefono">Telefono</label>
+                      <div class="input-group">
+                        <span class="input-group-addon" id="basic-addon1"><i class="glyphicon glyphicon-phone-alt"></i></span>
+                        <input type="number" class="form-control" placeholder="Digita qui il tuo numero di telefono" required>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-lg-6">
+                  <div class="form-group">
+                    <label for="exampleInputmessage">Messaggio</label>
+                    <textarea class="form-control" placeholder="Digita qui il tuo messaggio" rows="3" required></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-lg-12 text-center">
+                <button type="submit" class="btn btn-primary">Invia messaggio</button>
+              </div>
+            </div>
+
+          </form> 
+          <!-- form end -->
+
+          </body>
+        <script type="text/javascript" src="vendors/js/jquery.min.js"></script>
+        <script type="text/javascript" src="vendors/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.5.3/js/bootstrapValidator.min.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+        </html>
