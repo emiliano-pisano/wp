db757c419a71f729b7ac7d17859d84a5cc8afde1
emiliano-pisano <pisanoemiliano@gmail.com>
2017-05-23 00:51:31 +0200

    Update functions.php

--- wp-content/themes/pisano/functions.php
+++ wp-content/themes/pisano/functions.php
@@ -1,7 +1,8 @@
 <?php 
 
 function enqueue_css() { 
-	wp_enqueue_style( 'bs', get_template_directory_uri().'/vendors/css/bootstrap.min.css' );      wp_enqueue_style( 'st', get_template_directory_uri().'/style.css' ); 
+	wp_enqueue_style( 'bs', get_template_directory_uri().'/vendors/css/bootstrap.min.css' ); 
+	wp_enqueue_style( 'st', get_template_directory_uri().'/style.css' ); 
 } 
 add_action( 'wp_enqueue_scripts', 'enqueue_css' ); 
 
